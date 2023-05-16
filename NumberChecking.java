ae934e830ae2d8261aabb70f9964710f711764d9
Arko Kundu <109503991+arkokundu500@users.noreply.github.com>
2023-05-16 19:29:41 +0530

    Create NumberChecking.java

---
+++ NumberChecking.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class NumberChecker {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the value of M");
+        int M = sc.nextInt();
+        if (M % 3 == 0 && M % 5 == 0) {
+            System.out.println("Good Number");
+        } else if (M % 3 == 0 && M % 5 != 0) {
+            System.out.println("Bad Number");
+        } else if (M % 3 != 0 && M % 5 == 0) {
+            System.out.println("Poor Number");
+        } else {
+            System.out.println("-1");
+        }
+    }
+}
